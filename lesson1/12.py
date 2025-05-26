1664be9640ce6e6dc8f399fdd50f5e71b2fa7f73
olenka-ops <olenka.yatskovska@gmail.com>
2025-05-26 20:38:53 +0300

    23

---
+++ lesson1/12.py
@@ -0,0 +1,5 @@
+
+noun = input("введіть іменник")
+verb = input("введіть дієслово")
+adjectiv = input("введіть прикметник ")
+print("одного разу", adjectiv, noun, "вирішив", adjectiv, "через паркан")
